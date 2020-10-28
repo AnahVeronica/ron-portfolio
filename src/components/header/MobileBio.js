3c98d011bd9ac0aaa7eead9f159a82a1f7b20a91
arunsri7 <arun.i1997@gmail.com>
2020-10-28 23:56:46 +0530

    [mobileBio.js] Removed extra social media links from mobile view

--- src/components/header/MobileBio.js
+++ src/components/header/MobileBio.js
@@ -7,7 +7,7 @@ const MobileBio = (props) => {
 
     return (
         <div className="mobile-bio-main">
-            <img src={willjw3}  className="ml-4 mt-2" style={{ maxWidth: `50px`, maxHeight: `50px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
+            <img src={willjw3}  className="ml-4 mt-2" style={{ maxWidth: `75px`, maxHeight: `75px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
             <h4 className="mr-4 mt-4">{props.author}</h4>
         </div>
     )
